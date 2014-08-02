46318040d4d797cebf9721e73e9bb49423a73f84
Jan Paul <janpaul123@users.noreply.github.com>
2014-08-02 16:25:28 -0700

    Added Karma+Mocha+Chai testing

---
+++ karma.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    files: [
+      'ast_transformations.js',
+      '*_spec.js'
+    ],
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO, // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    autoWatch: true,
+    browsers: ['Chrome']
+  });
+};
