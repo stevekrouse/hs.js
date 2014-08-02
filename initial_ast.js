e30a87d055ea4567536c43ce3ac3179a7c6352ad
Jan Paul <janpaul123@users.noreply.github.com>
2014-08-02 16:22:25 -0700

    Added initialAST

---
+++ initial_ast.js
@@ -0,0 +1,15 @@
+window.initialAST = {
+  id: "29387492384",
+  type: "application",
+  functionName: {type: "functionName", name: "map"},
+  arguments: [
+    {type: "functionName", name: "(+ 1)"},
+    {type: "list", items: [
+      {type: "int", value: 1},
+      {type: "int", value: 2},
+      {type: "int", value: 3},
+      {type: "int", value: 4},
+      {type: "int", value: 5},
+    ]}
+  ]
+};
