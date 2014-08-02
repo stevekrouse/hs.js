ddefbd6e054edae1d8e82b84315025ed21e80faa
Jan Paul <janpaul123@users.noreply.github.com>
2014-08-02 16:55:59 -0700

    Implemented isApplicable for all the functions we need

--- ast_transformations_spec.js
+++ ast_transformations_spec.js
@@ -33,5 +33,53 @@ describe('ASTTransformations', function() {
       };
       chai.expect(ASTTransformations.isApplicable(AST, AST.id)).to.be.false;
     });
+
+    it('doesnt allow applying "unknown"', function() {
+      var AST = {
+        id: 1,
+        type: "application",
+        functionName: {id: 2, type: "functionName", name: "unknown"},
+        arguments: []
+      };
+      chai.expect(ASTTransformations.isApplicable(AST, AST.id)).to.be.false;
+    });
+
+    it('allows applying "map f []"', function() {
+      var AST = {
+        id: 1,
+        type: "application",
+        functionName: {id: 2, type: "functionName", name: "map"},
+        arguments: [
+          {id: 3, type: "functionName", name: "f"},
+          {id: 4, type: "list", items: []}
+        ]
+      };
+      chai.expect(ASTTransformations.isApplicable(AST, AST.id)).to.be.true;
+    });
+
+    it('allows applying "(+ 1) 1"', function() {
+      var AST = {
+        id: 1,
+        type: "application",
+        functionName: {id: 2, type: "functionName", name: "(+ 1)"},
+        arguments: [
+          {id: 3, type: "int", value: 1}
+        ]
+      };
+      chai.expect(ASTTransformations.isApplicable(AST, AST.id)).to.be.true;
+    });
+
+    it('allows applying "0 : []"', function() {
+      var AST = {
+        id: 1,
+        type: "application",
+        functionName: {id: 2, type: "functionName", name: ":", infix: true},
+        arguments: [
+          {id: 3, type: "int", value: 0},
+          {id: 3, type: "list", items: []}
+        ]
+      };
+      chai.expect(ASTTransformations.isApplicable(AST, AST.id)).to.be.true;
+    });
   });
 });
