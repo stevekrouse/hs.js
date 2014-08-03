82d9f08bf12a36e1fcc5f13f76bd1c2861f31a9b
stevekrouse <steveykrouse@gmail.com>
2014-08-03 00:57:12 -0700

    refactored pre-definted functions to seperate methods

---
+++ functions/cons.js
@@ -0,0 +1,28 @@
+window.cons = {
+  name: ':',
+  infix: true,
+  typeSignature: 'a -> [a] -> [a]',
+  isValidApplication: function(arguments) {
+    // TODO paramaterize list time
+    return arguments.length === 2 &&
+           arguments[1].type === 'list';
+  },
+  patterns: [
+    {
+      definitionLine: null,
+      doesMatch: function(arguments){
+        return true;
+      },
+      apply: function(arguments){
+        var items = arguments[1].items;
+        items.unshift(arguments[0]);
+
+        return {
+          id: uuid.v4(),
+          type: 'list',
+          items: items
+        };
+      }
+    }
+  ]
+};
