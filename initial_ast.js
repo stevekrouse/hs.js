54a6d182fc85c03dbf6e06344e50d5bee2e7e542
stevekrouse <skrouse@seas.upenn.edu>
2014-08-03 10:32:36 -0700

    add initial functions

--- initial_ast.js
+++ initial_ast.js
@@ -14,4 +14,16 @@ window.initialAST = {
   ]
 };
 
-window.initialFunctionDefinitions = "double :: a -> a\ndouble x = x + x\n";
+window.initialFunctionDefinitions = "map :: (a -> b) -> [a] -> [b]"            + "\n" +
+                                    "map f []     = []"                        + "\n" +
+                                    "map f (x:xs) = (f x) : map f xs"          + "\n" +
+                                    ""                                         + "\n" +
+                                    "fold :: (a -> b) -> b -> [a] -> [b]"      + "\n" +
+                                    "fold f i []     = []"                     + "\n" +
+                                    "fold f i (x:xs) = f x (fold f i xs)"      + "\n" +
+                                    ""                                         + "\n" +
+                                    "plus :: Int -> Int -> Int"                + "\n" +
+                                    "plus x y = x + y"                         + "\n" +
+                                    ""                                         + "\n" +
+                                    "double :: Int -> Int"                     + "\n" +
+                                    "double x = x + x"                         + "\n";
