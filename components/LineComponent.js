b8555f74cbe55ee05b7d4e0af894c36bcc9d5086
Jan Paul <janpaul123@users.noreply.github.com>
2014-08-02 21:17:02 -0700

    Pass lineState everywhere; prevent clicking earlier lines

--- components/LineComponent.js
+++ components/LineComponent.js
@@ -1,5 +1,6 @@
 var Line = React.createClass({displayName: 'Line',
   render: function() {
-    return React.DOM.div({className: "line"}, Node({ast: this.props.ast, id: this.props.ast.id}));
+    return React.DOM.div({className: "line"},
+      Node({lineState: this.props.lineState, id: this.props.lineState.ast.id}));
   }
 });
--- components/ListComponent.js
+++ components/ListComponent.js
@@ -1,10 +1,9 @@
 var List = React.createClass({displayName: 'List',
   mixins: [NodeMixins],
   render: function() {
-    var ast = this.props.ast;
-    items = this.currentAST().items.map(function(item){
-      return Node({ast: ast, id: item.id});
-    });
+    items = this.currentAST().items.map((function(item){
+      return Node({lineState: this.props.lineState, id: item.id});
+    }).bind(this));
     items.unshift('[');
     items.push(']');
     return React.DOM.span({className: "list"}, items);
