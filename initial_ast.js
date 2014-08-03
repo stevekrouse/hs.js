8c8d4674405c09abd854c7900438d44fcbc22645
stevekrouse <steveykrouse@gmail.com>
2014-08-03 13:08:03 -0700

    push fix for foldr foldl bugs

--- initial_ast.js
+++ initial_ast.js
@@ -2,13 +2,19 @@ window.initialFunctionDefinitions = "map :: (a -> b) -> [a] -> [b]"            +
                                     "map f []     = []"                        + "\n" +
                                     "map f (x:xs) = (f x) : map f xs"          + "\n" +
                                     ""                                         + "\n" +
-                                    "foldl :: (a -> b -> a) -> a -> [b] -> a"  + "\n" +
-                                    "foldl f i []     = i"                     + "\n" +
-                                    "foldl f i (x:xs) = f (foldl f i xs) x"    + "\n" +
-                                    ""                                         + "\n" +
                                     "foldr :: (a -> b -> b) -> b -> [a] -> b"  + "\n" +
                                     "foldr f i []     = i"                     + "\n" +
                                     "foldr f i (x:xs) = f x (foldr f i xs)"    + "\n" +
+                                    ""                                         + "\n" +
+                                    "foldl :: (a -> b -> a) -> a -> [b] -> a"  + "\n" +
+                                    "foldl f i []     = i"                     + "\n" +
+                                    "foldl f i (x:xs) = foldl f (f i x) xs"    + "\n" +
+                                    ""                                         + "\n" +
+                                    "cons :: Int -> [Int]"                     + "\n" +
+                                    "cons x xs = x : xs"                       + "\n" +
+                                    ""                                         + "\n" +
+                                    "reverseCons :: Int -> [Int]"              + "\n" +
+                                    "reverseCons xs x = x : xs"                + "\n" +
                                     ""                                         + "\n" +
                                     "plus :: Int -> Int -> Int"                + "\n" +
                                     "plus x y = x + y"                         + "\n" +
